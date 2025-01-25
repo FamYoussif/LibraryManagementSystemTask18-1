79625a172507a2be6ad4c1d4e329e216e81e06cd
LOQ <LOQ@DESKTOP-ROCA4IP>
2025-01-25 23:53:41 +0200

    Add project files.

---
+++ LibraryManagement/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LibraryManagement
+{
+    public class Book
+    {
+        public Book(string title, string author, string iSBN)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.ISBN = iSBN;
+            this.Availability = true;
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool Availability { get; set; }
+    }
+}
---
+++ LibraryManagement/Library.cs
@@ -0,0 +1,71 @@
+﻿namespace LibraryManagement
+{
+    public class Library
+{
+    private List<Book> books = new List<Book>();
+
+   
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+        Console.WriteLine($"Added '{book.Title}' to the library.");
+    }
+
+    // Search for a book 
+    public Book SearchBook(string title)
+    {
+        for (int i = 0; i < books.Count; i++)
+        {
+            if (books[i].Title.ToLower().Contains(title.ToLower()))
+            {
+                return books[i];
+            }
+        }
+        return null;
+    }
+
+    // Borrow a book
+    public void BorrowBook(string title)
+    {
+        Book book = SearchBook(title);
+        if (book != null && book.Availability)
+        {
+            book.Availability = false;
+            Console.WriteLine($"You have borrowed '{book.Title}'.");
+        }
+        else
+        {
+            if (book == null)
+            {
+                Console.WriteLine($"The book '{title}' is not in the library.");
+            }
+            else
+            {
+                Console.WriteLine($"The book '{book.Title}' is currently borrowed.");
+            }
+        }
+    }
+
+    // Return a book
+    public void ReturnBook(string title)
+    {
+        Book book = SearchBook(title);
+        if (book != null && !book.Availability)
+        {
+            book.Availability = true;
+            Console.WriteLine($"You have returned '{book.Title}'.");
+        }
+        else
+        {
+            if (book == null)
+            {
+                Console.WriteLine($"The book '{title}' is not in the library.");
+            }
+            else
+            {
+                Console.WriteLine($"The book '{book.Title}' is not borrowed.");
+            }
+        }
+    }
+}
+}
